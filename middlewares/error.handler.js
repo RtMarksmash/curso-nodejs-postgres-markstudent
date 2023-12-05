266886f07ba48dbd31c8e916416f82b5a19f0c7e
mario mora <jorgemariomoramolina@gmail.com>
2023-12-05 16:38:32 -0500

    finishing to connect mysql database

--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -1,23 +1,37 @@
-function logErrors (err, req, res, next) {
-  console.error(err);
-  next(err);
+const { ValidationError } = require("sequelize");
+
+function logErrors(err, req, res, next) {
+    console.error(err);
+    next(err);
+}
+
+function ormErrorHandler(err, req, res, next) {
+    if (err instanceof ValidationError) {
+        res.status(409).json({
+            statusCode: 409,
+            message: err.message,
+            error: err.errors,
+
+        })
+    }
+    next(err)
 }
 
 function errorHandler(err, req, res, next) {
-  res.status(500).json({
-    message: err.message,
-    stack: err.stack,
-  });
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    });
 }
 
 function boomErrorHandler(err, req, res, next) {
-  if (err.isBoom) {
-    const { output } = err;
-    res.status(output.statusCode).json(output.payload);
-  } else {
-    next(err);
-  }
+    if (err.isBoom) {
+        const { output } = err;
+        res.status(output.statusCode).json(output.payload);
+    } else {
+        next(err);
+    }
 }
 
 
-module.exports = { logErrors, errorHandler, boomErrorHandler }
+module.exports = { logErrors, ormErrorHandler, errorHandler, boomErrorHandler }
